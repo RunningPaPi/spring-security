87e5abdc9c747a4e32f26d2724cce09fb8026fb1
pydxmg <1196923457@qq.com>
2018-04-11 14:48:20 +0800

    个性化用户认证流程
    自定义登录页面：http.formLogin().loginPage("/imooc-signIn.html")

--- imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
+++ imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
@@ -1,18 +1,30 @@
 package com.imooc.security.browser;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import com.imooc.security.core.properties.SecurityProperties;
 
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter{
+	@Autowired
+	private SecurityProperties securityProperties;
+	
+	@Autowired
+	private AuthenticationSuccessHandler imoocAuthenticationSeccessHandler;
+	
+	@Autowired
+	private AuthenticationFailureHandler imoocAuthenticationFailureHandler;
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		
 		return new BCryptPasswordEncoder();
 	}
 	
@@ -23,9 +35,12 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter{
 		http.formLogin()
 			.loginPage("/authentication/require")
 			.loginProcessingUrl("/authentication/form")
+			.successHandler(imoocAuthenticationSeccessHandler)
+			.failureHandler(imoocAuthenticationFailureHandler)
 			.and()
 			.authorizeRequests()
-			.antMatchers("/authentication/require").permitAll()
+			.antMatchers("/authentication/require",
+					securityProperties.getBrowser().getLoginPage()).permitAll()
 			.anyRequest()
 			.authenticated()
 			.and()
--- imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityController.java
+++ imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.imooc.security.browser.support.SimpleResponse;
+import com.imooc.security.core.properties.SecurityProperties;
 
 @RestController
 public class BrowserSecurityController {
@@ -28,6 +30,9 @@ public class BrowserSecurityController {
 	private RequestCache requestCache = new HttpSessionRequestCache();
 
 	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+	
+	@Autowired
+	private SecurityProperties securityProperties;
 
 	@RequestMapping("/authentication/require")
 	@ResponseStatus(code = HttpStatus.UNAUTHORIZED)
@@ -38,8 +43,8 @@ public class BrowserSecurityController {
 		if (savedRequest != null) {
 			String targeturl = savedRequest.getRedirectUrl();
 			log.info("引发跳转的请求是：{}", targeturl);
-			if (StringUtils.endsWith(targeturl, ".html")) {
-				redirectStrategy.sendRedirect(request, response, "");
+			if (StringUtils.endsWithIgnoreCase(targeturl, ".html")) {
+				redirectStrategy.sendRedirect(request, response, securityProperties.getBrowser().getLoginPage());
 			}
 		}
 
