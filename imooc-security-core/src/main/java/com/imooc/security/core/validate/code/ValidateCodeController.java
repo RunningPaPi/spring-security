059c109a560ad3566b07dbf6afa1de63a1fe5c20
pydxmg <1196923457@qq.com>
2018-04-12 15:07:30 +0800

    课程里的验证码生成方法

--- imooc-security-core/src/main/java/com/imooc/security/core/validate/code/ValidateCodeController.java
+++ imooc-security-core/src/main/java/com/imooc/security/core/validate/code/ValidateCodeController.java
@@ -34,64 +34,56 @@ public class ValidateCodeController {
 	}
 
 	private ImageCode createImageCode() {
-		int width = 67;// 定义图片的width
-		int height = 23;// 定义图片的height
-		int codeCount = 4;// 定义图片上显示验证码的个数
-		int xx = 17;
-		int fontHeight = 18;
-		int codeY = 18;
-		char[] codeSequence = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890".toCharArray();
-		// 定义图像buffer
-		BufferedImage buffImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-		// Graphics2D gd = buffImg.createGraphics();
-		// Graphics2D gd = (Graphics2D) buffImg.getGraphics();
-		Graphics gd = buffImg.getGraphics();
-		// 创建一个随机数生成器类
+		int width = 67;
+		int height = 23;
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+		Graphics g = image.getGraphics();
+
 		Random random = new Random();
-		// 将图像填充为白色
-		gd.setColor(Color.WHITE);
-		gd.fillRect(0, 0, width, height);
 
-		// 创建字体，字体的大小应该根据图片的高度来定。
-		Font font = new Font("Fixedsys", Font.BOLD, fontHeight);
-		// 设置字体。
-		gd.setFont(font);
-
-		// 画边框。
-		gd.setColor(Color.BLACK);
-		gd.drawRect(0, 0, width - 1, height - 1);
-
-		// 随机产生40条干扰线，使图象中的认证码不易被其它程序探测到。
-		gd.setColor(Color.BLACK);
-		for (int i = 0; i < 30; i++) {
+		g.setColor(getRandColor(200, 250));
+		g.fillRect(0, 0, width, height);
+		g.setFont(new Font("Times New Roman", Font.ITALIC, 20));
+		g.setColor(getRandColor(160, 200));
+		for (int i = 0; i < 155; i++) {
 			int x = random.nextInt(width);
 			int y = random.nextInt(height);
 			int xl = random.nextInt(12);
 			int yl = random.nextInt(12);
-			gd.drawLine(x, y, x + xl, y + yl);
+			g.drawLine(x, y, x + xl, y + yl);
 		}
 
-		// randomCode用于保存随机产生的验证码，以便用户登录后进行验证。
-		StringBuffer randomCode = new StringBuffer();
-		int red = 0, green = 0, blue = 0;
-
-		// 随机产生codeCount数字的验证码。
-		for (int i = 0; i < codeCount; i++) {
-			// 得到随机产生的验证码数字。
-			String code = String.valueOf(codeSequence[random.nextInt(36)]);
-			// 产生随机的颜色分量来构造颜色值，这样输出的每位数字的颜色值都将不同。
-			red = random.nextInt(255);
-			green = random.nextInt(255);
-			blue = random.nextInt(255);
-
-			// 用随机产生的颜色将验证码绘制到图像中。
-			gd.setColor(new Color(red, green, blue));
-			gd.drawString(code, i * xx+3, codeY);
-
-			// 将产生的四个随机数组合在一起。
-			randomCode.append(code);
+		String sRand = "";
+		for (int i = 0; i < 4; i++) {
+			String rand = String.valueOf(random.nextInt(10));
+			sRand += rand;
+			g.setColor(new Color(20 + random.nextInt(110), 20 + random.nextInt(110), 20 + random.nextInt(110)));
+			g.drawString(rand, 13 * i + 6, 16);
 		}
 
-		return new ImageCode(buffImg, randomCode.toString(), 60);
+		g.dispose();
+
+		return new ImageCode(image, sRand, 60);
+	}
+	/**
+	 * 生成随机背景条纹
+	 * 
+	 * @param fc
+	 * @param bc
+	 * @return
+	 */
+	private Color getRandColor(int fc, int bc) {
+		Random random = new Random();
+		if (fc > 255) {
+			fc = 255;
+		}
+		if (bc > 255) {
+			bc = 255;
+		}
+		int r = fc + random.nextInt(bc - fc);
+		int g = fc + random.nextInt(bc - fc);
+		int b = fc + random.nextInt(bc - fc);
+		return new Color(r, g, b);
 	}
 }
